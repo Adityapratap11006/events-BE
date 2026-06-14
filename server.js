5a370c599c29b020f84b9ac4f80e9c5984bd29df
Adityapratap11006 <adityapratap11006@gmail.com>
2026-06-14 13:12:55 +0530

    Added root route

--- server.js
+++ server.js
@@ -8,5 +8,9 @@ app.use(cors());
 app.use(express.json());
 app.use("/", bookingRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Backend is up and running!");
+});
+
 const port = process.env.PORT || 9000;
 app.listen(port, () => console.log("Backend listening on", port));
